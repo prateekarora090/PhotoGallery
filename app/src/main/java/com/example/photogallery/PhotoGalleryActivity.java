ff504c77bf29a9770ae2ba7f489134360f5d7da5
prateekarora090 <prateek_arora@alumni.brown.edu>
2020-10-11 16:00:04 -0400

    changes for chapter 28

--- app/src/main/java/com/example/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/photogallery/PhotoGalleryActivity.java
@@ -3,9 +3,17 @@ package com.example.photogallery;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    //Added
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
+
     @Override
     protected Fragment createFragment() {
         return PhotoGalleryFragment.newInstance();
